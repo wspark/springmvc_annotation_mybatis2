b7d254692986a3d0278547b5e89c8eda3851034f
student <student@M15023>
2014-07-03 14:18:59 +0900

    --

--- src/main/java/com/biz/common/JDBCUtil.java
+++ src/main/java/com/biz/common/JDBCUtil.java
@@ -1,27 +1,28 @@
 package com.biz.common;
 
+import java.io.InputStream;
 import java.io.Reader;
 
-import com.ibatis.common.resources.Resources;
-//import org.apache.ibatis.io.Resources;
-import com.ibatis.sqlmap.client.SqlMapClient;
-import com.ibatis.sqlmap.client.SqlMapClientBuilder;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 public class JDBCUtil {
-	private static final SqlMapClient sqlMap;
+	private static final SqlSessionFactory sqlSessionFactory;
 	static {
 	try {
-		String resource = "SqlConfigMap.xml";
-		Reader reader = Resources.getResourceAsReader (resource);
-		sqlMap = SqlMapClientBuilder.buildSqlMapClient(reader); //xml parsing
+		String resource = "SqlConfigMap.xml";		
+		InputStream inputStream = Resources.getResourceAsStream(resource);
+		sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);		
+		
 	}catch (Exception e) {
 		e.printStackTrace();
 		throw new RuntimeException ("Error initializing	MyAppSqlConfig class. Cause: " + e);
 	
 		}
 	}
-	public static SqlMapClient getSqlMapInstance(){
-		return sqlMap;
+	public static SqlSessionFactory getSqlMapInstance(){
+		return sqlSessionFactory;
 	}
 	
 }
--- src/main/java/com/biz/user/dao/UserDAOImpl.java
+++ src/main/java/com/biz/user/dao/UserDAOImpl.java
@@ -3,10 +3,14 @@ package com.biz.user.dao;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import com.biz.user.vo.Users;
 import com.ibatis.sqlmap.client.SqlMapClient;
+
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
+
 import com.biz.common.JDBCUtil;
 
 @Repository
@@ -15,57 +19,52 @@ public class UserDAOImpl implements UserDAOIF {
 	@Override
 	public int insertUser(Users user) {
 	
-		SqlMapClient sqlMap = JDBCUtil.getSqlMapInstance();
+		SqlSession session = JDBCUtil.getSqlMapInstance().openSession(true);
 		
 		int row=-1;
 		try{
-			sqlMap.insert("insertUser",user);
+			session.insert("insertUser",user);
 			row=1;
-		}catch(Exception e){
-		e.printStackTrace();	
-		}
-	
-		return row;	
-		
+		}finally{
+			session.close();
+		}	
+		return row;			
 	}
 
 	@Override
 	public int updateUser(Users user) {
 		int row=-1;		
-		SqlMapClient sqlMap = JDBCUtil.getSqlMapInstance();
+		SqlSession session = JDBCUtil.getSqlMapInstance().openSession(true);
 		
 		try {
-			row = sqlMap.update("updateUser", user);
-		} catch (Exception e) {
-			e.printStackTrace();	
-		}
+			row = session.update("updateUser", user);
+		} finally{
+			session.close();
+		}	
 		return row;	
 	}
 
 	@Override
 	public int deleteUser(String id) {
 		int row=-1;		
-		SqlMapClient sqlMap = JDBCUtil.getSqlMapInstance();
+		SqlSession session = JDBCUtil.getSqlMapInstance().openSession(true);
 		
 		try {
-			row = sqlMap.delete("deleteUser", id);
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
+			row = session.delete("deleteUser", id);
+		}finally{
+			session.close();
+		}			
 		return row;
 	}
 
 	@Override
 	public Users findUser(String id) {
 		Users user=null;
-		SqlMapClient sqlMap = JDBCUtil.getSqlMapInstance();
+		SqlSession session = JDBCUtil.getSqlMapInstance().openSession(true);
 		try {
-			user = (Users)sqlMap.queryForObject("findUsers",id);
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			user = (Users)session.selectOne("findUsers",id);
+		} finally{
+			session.close();
 		}		
 		return user;
 	}
@@ -73,35 +72,33 @@ public class UserDAOImpl implements UserDAOIF {
 	@Override
 	public boolean login(String id, String password) {
 		boolean isExist=false;		
-		SqlMapClient sqlMap = JDBCUtil.getSqlMapInstance();
+		SqlSession session = JDBCUtil.getSqlMapInstance().openSession(true);
 		HashMap<String, String> loginInfo = new HashMap<String, String>();
 		loginInfo.put("id", id);
 		loginInfo.put("password",password);
-
 		
 		try {
-			Object result = sqlMap.queryForObject("login", loginInfo);
+			Object result = session.selectOne("login", loginInfo);
 			if(result != null ) isExist=true;
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return isExist;
-	
+		} finally{
+			session.close();
+		}	
+		return isExist;	
 	}
 
 	@Override
 	public ArrayList<Users> findUserList() {
 		ArrayList<Users> userList=new ArrayList<Users>();
-		SqlMapClient sqlMap = JDBCUtil.getSqlMapInstance();
+		SqlSession session = JDBCUtil.getSqlMapInstance().openSession(true);
 		
 		try {
-			userList = (ArrayList)sqlMap.queryForList("findUserList");
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
+			List<Users> result = session.selectList("findUserList");
+			for(Users user : result){
+				userList.add(user);
+			}
+		} finally{
+			session.close();
+		}			
 		return userList;
 	}
 }
